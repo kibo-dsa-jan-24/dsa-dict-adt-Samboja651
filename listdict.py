d7b4f824b925dc17f3b204227a7c32ad81748552
Samboja651 <waribekihiko651@gmail.com>
2024-01-13 09:42:31 +0300

    insert now working

--- listdict.py
+++ listdict.py
@@ -1,10 +1,20 @@
 class ListDictionary:
     def __init__(self):
         # implement the constructor
-        pass
+        self.dictionary = []
 
     def insert(self, key, value):
         # implement the insert() method
-        return False
+        if key in self.dictionary:
+            index = self.dictionary.index(key)
+            if self.dictionary[index + 1] == [value]:
+                return True
+            else:
+                self.dictionary[index + 1].insert(0, value)
+                self.dictionary[index + 1].pop(1)
+        else:
+            self.dictionary.append(key)
+            self.dictionary.append([value])
 
     # implement the rest of the dictionary methods below
+
